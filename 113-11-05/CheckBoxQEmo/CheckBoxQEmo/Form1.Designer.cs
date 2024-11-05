5aa101e672d1203e38b6d55accf2934a561fe3ef
A113221030 <A113221030@mail.shu.edu.tw>
2024-11-05 17:49:32 +0800

    113-11-05

---
+++ 113-11-05/CheckBoxQEmo/CheckBoxQEmo/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace CheckBoxQEmo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            checkBox4 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            groupBox2 = new GroupBox();
+            checkBox5 = new CheckBox();
+            checkBox6 = new CheckBox();
+            checkBox7 = new CheckBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(checkBox4);
+            groupBox1.Controls.Add(checkBox3);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Location = new Point(5, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(258, 296);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "主餐";
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(90, 195);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(50, 19);
+            checkBox4.TabIndex = 7;
+            checkBox4.Text = "比薩";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(90, 150);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(50, 19);
+            checkBox3.TabIndex = 6;
+            checkBox3.Text = "牛排";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(90, 99);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(50, 19);
+            checkBox2.TabIndex = 5;
+            checkBox2.Text = "炸雞";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(90, 49);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(50, 19);
+            checkBox1.TabIndex = 4;
+            checkBox1.Text = "漢堡";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(checkBox7);
+            groupBox2.Controls.Add(checkBox6);
+            groupBox2.Controls.Add(checkBox5);
+            groupBox2.Location = new Point(281, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(253, 296);
+            groupBox2.TabIndex = 6;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "飲料";
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(85, 50);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(50, 19);
+            checkBox5.TabIndex = 5;
+            checkBox5.Text = "可樂";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new Point(85, 89);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(50, 19);
+            checkBox6.TabIndex = 6;
+            checkBox6.Text = "雪碧";
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.Location = new Point(85, 131);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(62, 19);
+            checkBox7.TabIndex = 7;
+            checkBox7.Text = "冰咖啡";
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoValidate = AutoValidate.EnableAllowFocusChange;
+            ClientSize = new Size(757, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "V";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private CheckBox checkBox4;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private GroupBox groupBox2;
+        private CheckBox checkBox7;
+        private CheckBox checkBox6;
+        private CheckBox checkBox5;
+    }
+}
